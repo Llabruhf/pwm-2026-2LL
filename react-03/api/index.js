ed7c1a2a81c7d15da0d108c00546184d38036b8b
Llabruhf <70967427+Llabruhf@users.noreply.github.com>
2026-09-22 11:33:50 +0000

    Alteração banco

--- react-03/api/index.js
+++ react-03/api/index.js
@@ -6,8 +6,8 @@ const instance = axios.create({
   baseURL: "https://parseapi.back4app.com",
   timeout: 5000,
   headers: {
-    "X-Parse-Application-Id": "t4wZSEfltZ4QtFBZX5LY9hyyx4I45uAbjsrwzCAi",
-    "X-Parse-REST-API-Key": "ZXx7xAlvFOyZith0HDnSpWdlHxnnATKsdgakYg4Q",
+    "X-Parse-Application-Id": "vp6XcBFFUTJHEqr8uDAUs15sYXvjmvVroe99OWhF",
+    "X-Parse-REST-API-Key": "vrgRVhDHxaoovqoyhlx3ialgAyWNoqPumJXHalSt",
   },
 });
 
